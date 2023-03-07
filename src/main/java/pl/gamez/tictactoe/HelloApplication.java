dbba02f8fde0c65689791ea70d25d2cd56ba1541
Melak <km250542@ncr.com>
2023-03-07 08:25:34 +0100

    Positions of all buttons are correct

--- src/main/java/pl/gamez/tictactoe/HelloApplication.java
+++ src/main/java/pl/gamez/tictactoe/HelloApplication.java
@@ -30,30 +30,42 @@ public class HelloApplication extends Application {
         Image iconToPlay = new Image("img/square.png");
 
 
-        primaryStage.getIcons().add(icon);
+
 
         ImageView imageView = new ImageView(iconToPlay);
         imageView.setFitHeight(411);
         imageView.setFitWidth(411);
 
-        Button button = new MyButton("Button");
-        Button button2 = new MyButton("Button2");
+        Button button = new MyButton("", 12, 12);
+        Button button2 = new MyButton("", 146, 12);
+        Button button3 = new MyButton("", 280,12);
 
-        button.setLayoutX(12);
-        button.setLayoutY(12);
+        Button button4 = new MyButton("", 12, 146);
+        Button button5 = new MyButton("", 146, 146);
+        Button button6 = new MyButton("", 280,146);
 
-        button2.setLayoutX(56);
-        button2.setLayoutY(12);
+        Button button7 = new MyButton("", 12, 280);
+        Button button8 = new MyButton("", 146, 280);
+        Button button9 = new MyButton("", 280,280);
+
+//        button.setLayoutX(12);
+//        button.setLayoutY(12);
+//
+//        button2.setLayoutX(56);
+//        button2.setLayoutY(12);
 
 
-        VBox buttBox = new VBox();
-        buttBox.getChildren().addAll(button, button2);
-        Group root = new Group(imageView, buttBox);
+//        VBox buttBox = new VBox();
+//        buttBox.getChildren().addAll(button, button2);
+        Group root = new Group(imageView,
+                button, button2, button3,
+                button4, button5, button6,
+                button7, button8, button9);
         Scene scene = new Scene(root, Color.GRAY);
 
 
 
-
+        primaryStage.getIcons().add(icon);
         primaryStage.setTitle("Tic Tac Toe GAME");
         primaryStage.setResizable(false);
         primaryStage.setWidth(600);
--- src/main/java/pl/gamez/tictactoe/buttons/MyButton.java
+++ src/main/java/pl/gamez/tictactoe/buttons/MyButton.java
@@ -5,8 +5,10 @@ import javafx.scene.control.Button;
 
 public class MyButton extends Button {
 
-    public MyButton(String name) {
+    public MyButton(String name, int x, int y) {
         super(name);
+        this.setLayoutX(x);
+        this.setLayoutY(y);
         this.setPrefHeight(121);
         this.setPrefWidth(121);
         this.setOpacity(0.5);
