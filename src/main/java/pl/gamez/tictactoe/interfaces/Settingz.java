4863e8554aecf08b470b8b712fd565c03fcca181
Melak <km250542@ncr.com>
2023-03-13 14:29:22 +0100

    Reset Button

--- src/main/java/pl/gamez/tictactoe/interfaces/Settingz.java
+++ src/main/java/pl/gamez/tictactoe/interfaces/Settingz.java
@@ -1,10 +1,23 @@
 package pl.gamez.tictactoe.interfaces;
 
+//import pl.gamez.tictactoe.ChangeStatusClass;
+import pl.gamez.tictactoe.GameSetting;
+
 public interface Settingz {
 
     void setValue(int i, int j, int v);
 
-    boolean checkWhoWin();
+    void restartState();
+
+    void setWinX();
+
+    void setWinO();
+
+    //void notifyGameStatus();
+
+    boolean checkWhoWin() throws InterruptedException;
 
     int[][] getGameArray();
+
+
 }
